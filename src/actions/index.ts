4714c22a1f5fa2bfcc6d8400d02a7c0c487be7d9
SahakK <sahak.keshishyan.dev@gmail.com>
2024-05-26 00:35:03 +0400

    create routing structure, path helpers, stubbing out server actions

---
+++ src/actions/index.ts
@@ -0,0 +1,8 @@
+"use server";
+
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+
+export { createPost } from "./create-post";
+export { createComment } from "./create-comment";
+export { createTopic } from "./create-topic";
--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,7 @@ import { prisma } from "@/db";
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
-if (GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
   throw new Error("Missing github oauth credentials");
 }
 
