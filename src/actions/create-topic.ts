15affd8a5c1fd565fab5ea55254c6e890e072a1d
SahakK <sahak.keshishyan.dev@gmail.com>
2024-05-29 07:43:59 +0400

    create a topic functionality in progress, validate form with Zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,29 @@
 "use server";
 
-export async function createTopic() {
+import { z } from "zod";
+
+const topicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const name = formData.get("name");
+  const description = formData.get("description");
+
+  const result = topicSchema.safeParse({
+    name,
+    description,
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+
   // TODO: ravalidate the homepage
 }
