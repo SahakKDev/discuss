6c1e3634d7f15726d21080310f72bb0b946d1b8b
SahakK <sahak.keshishyan.dev@gmail.com>
2024-05-30 07:41:33 +0400

    create topic functionality done, handle auth and db errors, display error messages in form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,20 @@
 "use server";
 
 import { z } from "zod";
+import { auth } from "@/auth";
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { prisma } from "@/db";
+import paths from "@/paths";
+import { revalidatePath } from "next/cache";
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
 
 const topicSchema = z.object({
   name: z
@@ -12,7 +26,10 @@ const topicSchema = z.object({
   description: z.string().min(10),
 });
 
-export async function createTopic(formData: FormData) {
+export async function createTopic(
+  _formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const name = formData.get("name");
   const description = formData.get("description");
 
@@ -22,8 +39,45 @@ export async function createTopic(formData: FormData) {
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
   }
 
-  // TODO: ravalidate the homepage
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["Please log in to create a new topic."],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await prisma.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    }
+
+    return {
+      errors: {
+        _form: ["Something went wrong."],
+      },
+    };
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
